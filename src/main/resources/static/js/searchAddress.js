85d4466df38929654e7230e63a9623b1165daa83
kim-chaeyeon <20212621@gm.hannam.ac.kr>
2024-06-19 23:10:46 +0900

    주소 클릭하면 지도가 자동으로 바뀌는 것 까지 성공

--- src/main/resources/static/js/searchAddress.js
+++ src/main/resources/static/js/searchAddress.js
@@ -46,3 +46,84 @@ $(document).ready(function() {
         });
     });
 });
+
+$(document).ready(function() {
+    var mapContainer = document.getElementById('map'); // 지도를 표시할 div
+    var mapOption = {
+        center: new kakao.maps.LatLng(33.450701, 126.570667), // 지도의 중심좌표
+        level: 3 // 지도의 확대 레벨
+    };
+
+    // 지도를 생성합니다
+    var map = new kakao.maps.Map(mapContainer, mapOption);
+
+    // 주소-좌표 변환 객체를 생성합니다
+    var geocoder = new kakao.maps.services.Geocoder();
+
+    // 지도에 표시할 마커를 생성합니다
+    var marker = new kakao.maps.Marker({
+        map: map
+    });
+
+    function fetchAddresses(query) {
+        $.ajax({
+            url: '/naver/search',
+            type: 'GET',
+            data: {
+                query: query
+            },
+            success: function(data) {
+                var addressList = $('#addressList');
+                addressList.empty();
+
+                if (data && data.items && Array.isArray(data.items)) {
+                    data.items.forEach(function(item) {
+                        var addressItem = $('<div></div>')
+                            .text(item.address)
+                            .addClass('address-item')
+                            .css({
+                                cursor: 'pointer',
+                                padding: '5px',
+                                border: '1px solid #ddd',
+                                margin: '5px 0'
+                            });
+
+                        addressItem.click(function() {
+                            var selectedAddress = $(this).text();
+                            $('#address').val(selectedAddress);
+
+                            // 주소로 좌표를 검색합니다
+                            geocoder.addressSearch(selectedAddress, function(result, status) {
+                                if (status === kakao.maps.services.Status.OK) {
+                                    var coords = new kakao.maps.LatLng(result[0].y, result[0].x);
+
+                                    // 결과값으로 받은 위치를 마커로 표시합니다
+                                    marker.setPosition(coords);
+                                    marker.setMap(map);
+
+                                    // 지도의 중심을 결과값으로 받은 위치로 이동시킵니다
+                                    map.setCenter(coords);
+                                }
+                            });
+                        });
+
+                        addressList.append(addressItem);
+                    });
+                } else {
+                    console.error('API 응답 데이터 형식 오류:', data);
+                    alert('주소 목록을 불러오는 데 문제가 발생했습니다.');
+                }
+            },
+            error: function() {
+                alert('주소를 불러오는 데 실패했습니다.');
+            }
+        });
+    }
+
+    $('#searchAddress').click(function() {
+        var query = $('#restaurantName').val().trim();
+        if (query) {
+            fetchAddresses(query);
+        }
+    });
+});
